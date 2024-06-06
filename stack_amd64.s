a867182ef2a3a1746b65d96ab2058d9a74b4913b
黎显图 <lxt1045@gmail.com>
2024-06-06 10:59:59 +0800

    feat: pc -=1

--- stack_amd64.s
+++ stack_amd64.s
@@ -35,7 +35,7 @@ TEXT ·getPC(SB),NOSPLIT,$0-8
 	// 返回上一层调用栈的 pc; 由于getPC栈帧为0，所以没有压入BP，这里就少一层BP
 	// 理论上，不压入BP的平台(非arm64、X64)也可以如此获取，就是要改成 "MOVQ	+0(BP), AX"，因为有BP才需要+8
 	MOVQ	+8(BP), AX		
-	// SUBQ	$1, AX          // pc-1 才是真正的PC，但是 runtime.Callers 并没有这样做，为了一致性，这里先注释了
+	SUBQ	$1, AX          // pc-1 才是真正的PC，但是 runtime.Callers 并没有这样做，为了一致性，这里先注释了
 	MOVQ	AX, ret+0(FP)
 	RET
 
@@ -44,7 +44,7 @@ TEXT ·getPC(SB),NOSPLIT,$0-8
 TEXT ·GetPC(SB),NOSPLIT,$0-8
 	NO_LOCAL_POINTERS
 	MOVQ	+8(BP), AX		// 返回上一层调用栈的 pc
-	// SUBQ	$1, AX
+	SUBQ	$1, AX
 	MOVQ	AX, ret+0(FP)
 	RET
 
@@ -61,7 +61,7 @@ loop:
 	JAE	return				// 无符号大于等于就跳转
 
 	MOVQ	+8(BP), BX		// last pc -> BX
-	// SUBQ	$1, BX          //  pc-1 才是真正的PC，但是 runtime.Callers 并没有这样做
+	SUBQ	$1, BX          //  pc-1 才是真正的PC，但是 runtime.Callers 并没有这样做
 	MOVQ	BX, 0(AX)(CX*8)		// s[i] = BX
 	
 	ADDQ	$1, CX			// CX++ / i++
@@ -93,7 +93,7 @@ loop:
 	JAE	return				// 无符号大于等于就跳转
 
 	MOVQ	+8(BP), BX		// last pc -> BX
-	// SUBQ	$1, BX
+	SUBQ	$1, BX
 	MOVQ	BX, 0(AX)(CX*8)		// s[i] = BX
 	
 	MOVQ	+0(BP), BP 		// last BP; 展开调用栈至上一层
