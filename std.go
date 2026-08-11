894ca1ba9d4235bebcd5246cc11e8e3c7020f88d
lxt\lixiantu <lxt1045@163.com>
2026-08-11 10:00:30 +0800

    feat:

--- std.go
+++ std.go
@@ -9,11 +9,30 @@ const (
 	DefaultMsg  = ""
 )
 
-//Is 检查code是不是一样的
+// Is 检查code是不是一样的
 func Is(err1, target error) bool {
 	e1, ok := err1.(*Code)
 	if !ok || e1.Code() == DefaultCode {
 		return stderrs.Is(err1, target)
 	}
 	return e1.Is(target)
+}
+
+func WithErr(err error) (e *Code) {
+	e, ok := err.(*Code)
+	if ok {
+		return
+	}
+	return NewCode(1, DefaultCode, err.Error())
+}
+
+func AsCode(err error) (e *Code) {
+	e, ok := err.(*Code)
+	if ok {
+		return
+	}
+	return &Code{
+		code: DefaultCode,
+		msg:  err.Error(),
+	}
 }
